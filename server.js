97330acbf52c38487d5d13d6a33162ddee42d365
Callum3574 <callum.hall123@hotmail.com>
2023-02-07 17:59:49 +0000

    implemented communication with backend for new input

--- server.js
+++ server.js
@@ -1,34 +1,32 @@
 const express = require("express");
 const app = express();
 const client = require("./database.js");
-
 const port = 4000 || process.env.PORT;
+app.use(express.json());
+//cors
+const cors = require("cors");
+app.use(cors(["http://localhost:3000"]));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("/input_exercise", (req, res) => {
-  const text = "INSERT INTO table_name VALUES ($1, $2, $3, $4, $5)";
-  const values = [
-    req.body.type,
-    req.body.distance,
-    req.body.steps,
-    req.body.calories,
-    req.body.time,
-  ];
-  console.log(values);
+app.post("/input_exercise", async (req, res) => {
+  try {
+    const data = await req.body;
+    const query = {
+      text: "INSERT INTO workout VALUES ($1, $2, $3, $4)",
+      values: [data.type, data.distance, data.steps, data.calories],
+    };
+    client.query(query);
+    console.log(data);
 
-  client.query(text, values, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(result.rows);
-  });
-
-  console.log(res.message);
+    res.status(200).send({ message: "successful input!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "server error!" });
+  }
 });
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
