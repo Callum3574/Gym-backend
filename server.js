9817c90ce667125a4b676a79b7f652a2a44277dd
Callum3574 <callum.hall123@hotmail.com>
2023-02-08 23:17:20 +0000

    normalised DB and joined tables for db queries

--- server.js
+++ server.js
@@ -3,30 +3,43 @@ const app = express();
 const client = require("./database.js");
 const port = 4000 || process.env.PORT;
 app.use(express.json());
-//cors
 const cors = require("cors");
 app.use(cors(["http://localhost:3000"]));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.post("/input_exercise", async (req, res) => {
   try {
     const data = await req.body;
     const query = {
-      text: "INSERT INTO workout VALUES ($1, $2, $3, $4)",
-      values: [data.type, data.distance, data.steps, data.calories],
+      text: "INSERT INTO exercises (workout_id, duration, calories, steps, date, distance) VALUES ($1, $2, $3, $4, $5, $6)",
+      values: [
+        data.exercise_id,
+        data.duration,
+        data.calories,
+        data.steps,
+        data.date,
+        data.distance,
+      ],
     };
     client.query(query);
     console.log(data);
-
-    res.status(200).send({ message: "successful input!" });
   } catch (error) {
     console.log(error);
     res.status(500).send({ message: "server error!" });
   }
 });
+
+app.get("/all_walk_data", async (req, res) => {
+  try {
+    const data = await client.query(
+      "SELECT distance, date, steps, calories, duration, type FROM exercises JOIN workouts ON workouts.id = exercises.workout_id ORDER BY workouts.id DESC"
+    );
+    res.status(200).json(data.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ message: "server error!" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
